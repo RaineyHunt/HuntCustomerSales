45e36b83944858a14519af869fa08a2aa53c0775
343428140 <343428140@C-2UA4081328.tdsb.on.ca>
2022-05-30 13:29:50 -0400

    end of first day

--- BuisnessAnalysis/src/hunt/rainey/Customer.java
+++ BuisnessAnalysis/src/hunt/rainey/Customer.java
@@ -1,5 +1,24 @@
 package hunt.rainey;
 
 public class Customer {
-
+	private String first;
+	private String last;
+	private int id;
+	private String city;
+	private String pCode;
+	private int cCard;
+	
+	Customer(String s, String a, String d, String g, int h){
+		first = s;
+		last = a;
+		id = 1;
+		city = d;
+		pCode = g;
+		cCard = h;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Customer ID: %d %nName: %s %s %nCity: %s Postal Code: %s", id,first,last,city, pCode);
+	}
 }
--- BuisnessAnalysis/src/hunt/rainey/Driver.java
+++ BuisnessAnalysis/src/hunt/rainey/Driver.java
@@ -1,10 +1,45 @@
 package hunt.rainey;
 
+import java.util.Scanner;
+
 public class Driver {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter your first name");
+		String a = in.next();
+		System.out.println("Please enter your last name");
+		String b = in.next();
+		System.out.println("Please enter your City of residence");
+		String c = in.next();
+		String d = "";
+		int e = 0;
+		boolean valid = false;
+		while(!valid) {
+			System.out.println("Please enter your postal code");
+			d = in.next();
+			if(pCodeVeri(d)) {
+				valid = true;
+			}
+			
+		}
+		valid = false;
+		while(!valid) {
+			System.out.println("Please enter your credit card number");
+			e = in.nextInt();
+			if(cCardVeri(e)) {
+				valid = true;
+			}
+		}
+		Customer w = new Customer(a,b,c,d,e);
+		System.out.println(w.toString());
+	}
+	public static boolean pCodeVeri(String x) {
+		return true;
+	}
+	
+	public static boolean cCardVeri(int x) {
+		return true;
 	}
 
 }
